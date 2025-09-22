262d31ed47a438eb34d768d6be1d4bc04ff9a262
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-09-22 10:39:23 +0300

    remove Spotify values from YTM script

--- src/scripts/retrieve-releases-youtube-music/environment.ts
+++ src/scripts/retrieve-releases-youtube-music/environment.ts
@@ -7,8 +7,6 @@ const environmentSchema = object({
   BLUESKY_IDENTIFIER_FOR_YOUTUBE_MUSIC: pipe(string(), minLength(5)),
   BLUESKY_PASSWORD_FOR_YOUTUBE_MUSIC: pipe(string(), minLength(8)),
   LASTFM_API_KEY: pipe(string(), nonEmpty()),
-  SPOTIFY_CLIENT_ID: pipe(string(), nonEmpty()),
-  SPOTIFY_CLIENT_SECRET: pipe(string(), nonEmpty()),
   TELEGRAM_BOT_TOKEN: pipe(string(), nonEmpty()),
   TELEGRAM_CHANNEL_FOR_YOUTUBE_MUSIC: pipe(string(), nonEmpty()),
 });
